0967cce78e7562cfdc01a228985b9f17a0db42a5
Rajveer-7 <sin12051@sheridancollege.ca>
2023-01-26 00:48:06 -0500

    First Commit

---
+++ src/gitdemo/GitDemo.java
@@ -0,0 +1,18 @@
+package gitdemo;
+
+/**
+ * This class +++ Insert class Description here 
+ *
+ * @author Rajveer Singh
+ */
+public class GitDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is Git Demo");
+        
+    }
+
+}
